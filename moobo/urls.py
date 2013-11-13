cfa443823c2bf81b57fa739bcdd11bb4d75d74bc
puras <puras.he@gmail.com>
2013-11-13 08:36:19 +0800

    增加默认根路径指向

--- moobo/urls.py
+++ moobo/urls.py
@@ -2,6 +2,8 @@
 __author__ = 'puras'
 from django.conf.urls import patterns, include, url
 
+from bbs.views import index
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,6 +11,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'moobo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', index),
     url(r'^bbs/$', include('bbs.urls', namespace='bbs')),
     # url(r'^blog/$', include(blog.urls, namespace='blog')),
     url(r'^admin/', include(admin.site.urls)),

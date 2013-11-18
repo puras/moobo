7cabdfad199dfa72c03d89ee8390abdc240fa0c8
puras <puras.he@gmail.com>
2013-11-18 14:09:11 +0800

    增加用户注册、登录与注销功能

--- bbs/urls.py
+++ bbs/urls.py
@@ -7,4 +7,7 @@ from bbs import views
 urlpatterns = patterns('', 
     url(r'^$', views.index, name='index'),
     url(r'^topic/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^join/$', views.join, name='join'),
 )
--- bbs/views.py
+++ bbs/views.py
@@ -2,8 +2,12 @@
 __author__ = 'puras'
 
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+
+from django import forms
+from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm
 
 from bbs.models import Topic, Node, Category
 
@@ -16,3 +20,50 @@ def index(req):
 def topic(req, topic_id):
     topic = get_object_or_404(Topic, pk = topic_id)
     return render(req, 'topic.html', {'topic': topic})
+
+def login(req):
+    if req.method == 'POST':
+        username = req.POST.get('username', '')
+        passwd = req.POST.get('passwd', '')
+        user = auth.authenticate(username = username, password = passwd)
+        if user is not None and user.is_active:
+            auth.login(req, user)
+            # return HttpResponse('You are logged in.')
+            return HttpResponseRedirect('/')
+        else:
+            return render(req, 'login.html')
+            # return HttpResponse('You are not logged in.')
+
+    return render(req, 'login.html')
+    # if req.method == 'POST':
+        # if req.session.test_cookie_worked():
+        #     req.session.delete_test_cookie()
+
+        #     return HttpResponse('You are logged in.')
+        # else:
+        #     return HttpResponse('Please enable cookies and try again.')
+
+    # req.session.set_test_cookie()
+    # return render(req, 'login.html')
+
+def logout(req):
+    auth.logout(req)
+    return HttpResponseRedirect(reverse('bbs:login'))
+
+def join(req):
+    if req.method == 'POST':
+        form = UserCreationForm(req.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = UserCreationForm()
+        return render(req, 'join.html', { 'form': form, })
+    # if req.method == 'POST':
+    #     if req.session.test_cookie_worked():
+    #         return HttpResponse('You are sign up ok.')
+    #     else:
+    #         return HttpResponse('Please enable cookies and try again.')
+
+    # req.session.set_test_cookie()
+    # return render(req, 'join.html')

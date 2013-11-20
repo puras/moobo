93b46ed789afc09def93de2987fa7109059604c0
puras <puras.he@gmail.com>
2013-11-20 12:57:55 +0800

    主题标题、内容和回复内容，要大于4个字符

--- bbs/forms.py
+++ bbs/forms.py
@@ -7,5 +7,23 @@ class TopicForm(forms.Form):
     title = forms.CharField(widget = forms.TextInput(attrs={'size': '100', 'placeholder': '主题标题'}))
     content = forms.CharField(widget=forms.Textarea(attrs={'cols': '100', 'rows': '30', 'placeholder': '主题内容'}))
 
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) < 4:
+            raise forms.ValidationError('内容过短')
+        return title
+
+    def clean_content(self):
+        content = self.cleaned_data['content']
+        if len(content) < 4:
+            raise forms.ValidationError('内容过短')
+        return content
+
 class ReplyForm(forms.Form):
     content = forms.CharField(widget=forms.Textarea(attrs={'cols': '100', 'rows': '10', 'placeholder': '回复内容'}))
+    
+    def clean_content(self):
+        content = self.cleaned_data['content']
+        if len(content) < 4:
+            raise forms.ValidationError('内容过短')
+        return content

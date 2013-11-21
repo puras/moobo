95ec1aefd7127f972c8716853fa8fe5f9175a0eb
puras <puras.he@gmail.com>
2013-11-21 09:43:23 +0800

    修改主题发布页面样式

--- bbs/forms.py
+++ bbs/forms.py
@@ -4,8 +4,8 @@ __author__ = 'puras'
 from django import forms
 
 class TopicForm(forms.Form):
-    title = forms.CharField(widget = forms.TextInput(attrs={'size': '100', 'placeholder': '主题标题'}))
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': '100', 'rows': '30', 'placeholder': '主题内容'}))
+    title = forms.CharField(widget = forms.TextInput(attrs={'size': '90', 'placeholder': '主题标题'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': '90', 'rows': '20', 'placeholder': '主题内容'}))
 
     def clean_title(self):
         title = self.cleaned_data['title']
@@ -20,7 +20,7 @@ class TopicForm(forms.Form):
         return content
 
 class ReplyForm(forms.Form):
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': '100', 'rows': '10', 'placeholder': '回复内容'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': '90', 'rows': '10', 'placeholder': '回复内容'}))
 
     def clean_content(self):
         content = self.cleaned_data['content']
